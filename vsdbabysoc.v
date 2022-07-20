504587af5271d090483230c7ec2847e7d5994e33
ireneann713 <55539862+ireneann713@users.noreply.github.com>
2022-07-20 21:01:26 +0530

    Add files via upload

---
+++ vsdbabysoc.v
@@ -0,0 +1,44 @@
+`include "rvmyth.v"
+`include "avsdpll.v"
+`include "avsddac.v"
+
+
+module vsdbabysoc (
+   output wire OUT,
+   //
+   input  wire reset,
+   //
+   input  wire VCO_IN,
+   input  wire ENb_CP,
+   input  wire ENb_VCO,
+   input  wire REF,
+   //
+   // input  wire VREFL,
+   input  wire VREFH
+);
+
+   wire CLK;
+   wire [9:0] RV_TO_DAC;
+
+   rvmyth core (
+      .out(RV_TO_DAC),
+      .clk(CLK),
+      .reset(reset)
+   );
+
+   avsdpll pll (
+      .CLK(CLK),
+      .VCO_IN(VCO_IN),
+      .ENb_CP(ENb_CP),
+      .ENb_VCO(ENb_VCO),
+      .REF(REF)
+   );
+
+   avsddac dac (
+      .OUT(OUT),
+      .D(RV_TO_DAC),
+      // .VREFL(VREFL),
+      .VREFH(VREFH)
+   );
+   
+endmodule
